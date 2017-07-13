2a46b927b8e9b638d8318fa522cbbb76461aaf22
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2017-07-13 14:05:49 -0400

    Program class modified

--- COMP123-s2017-Lesson9B-Lab24/Program.cs
+++ COMP123-s2017-Lesson9B-Lab24/Program.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/* Name: Bishnu Khanal
+ * Date: July 11, 2017
+ * Description: COMP123 Lesson09 Lab 24
+ * Version:0.4 - Program class modified
+ */
 namespace COMP123_s2017_Lesson9B_Lab24
 {
     class Program
