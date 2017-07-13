12c0fb39280f90adf903c547a6f59cfec979f813
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2017-07-13 14:38:30 -0400

    Modified Human class as Abstract class

---
+++ COMP123-s2017-Lesson9B-Lab24/SuperHuman.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/* Name: Bishnu Khanal
+ * Date: July 11, 2017
+ * Description: This is the SuperHuman class
+ * Version:0.1 - Created the SuperHuman class
+ */
+namespace COMP123_s2017_Lesson9B_Lab24
+{
+    public class SuperHuman
+    {
+    }
+}
