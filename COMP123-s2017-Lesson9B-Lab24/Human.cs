cdc22e7576b1365409e6e9918fc5728d28a2fea0
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2017-07-13 14:13:45 -0400

    Created and modified Human Class

---
+++ COMP123-s2017-Lesson9B-Lab24/Human.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/* Name: Bishnu Khanal
+ * Date: July 11, 2017
+ * Description: This is the abstract human class
+ * Version:0.1 - Created the Abstract Human class
+ */
+namespace COMP123_s2017_Lesson9B_Lab24
+{
+    /// <summary>
+    /// This is the Human class
+    /// </summary>
+    public abstract class Human
+    {
+        //Private instant variables
+        private string _name;
+
+        //Public properties
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        }
+        //Constructors
+        /// <summary>
+        /// This is the main constructor for the Human class.
+        /// It takes one parameter -name(string)
+        /// </summary>
+        /// <param name="name"></param>
+        public Human(string name)
+        {
+            this.Name = name;
+        }
+        //public abstracts methods
+        public void DisplaySkills()
+        {
+        }
+    }
+}
