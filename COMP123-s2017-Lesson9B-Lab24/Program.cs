ba4749f02495a35722971d638abefc57a7b5cc26
Admin <Admin@DESKTOP-MQINT8U>
2017-07-13 14:02:07 -0400

    Add project files.

---
+++ COMP123-s2017-Lesson9B-Lab24/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_s2017_Lesson9B_Lab24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
