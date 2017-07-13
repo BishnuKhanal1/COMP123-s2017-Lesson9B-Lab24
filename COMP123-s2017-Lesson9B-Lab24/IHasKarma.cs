239f4ea229638be19800a6c721e8293f407b1358
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2017-07-13 15:44:47 -0400

    Modified IHasKarma Interface

--- COMP123-s2017-Lesson9B-Lab24/IHasKarma.cs
+++ COMP123-s2017-Lesson9B-Lab24/IHasKarma.cs
@@ -5,11 +5,16 @@ using System.Text;
 /* Name: Bishnu Khanal
  * Date: July 13, 2017
  * Description: This is the IHasKarma Interface
- * Version:0.1 - Created IHasKarma Interface            
+ * Version:0.2 - IHasKarma Interface modified       
  */
 namespace COMP123_s2017_Lesson9B_Lab24
 {
+    /// <summary>
+    /// This is IHasKarma Interface
+    /// </summary>
     public interface IHasKarma
     {
+        //public properties
+        int Karma { get; set; } //intefaces contains only property
     }
 }
